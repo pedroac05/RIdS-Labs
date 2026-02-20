23eb0f58d245e4f48b51fed49077ed5665dd5cff
jecamposa <103275887+jecamposa@users.noreply.github.com>
2026-02-19 20:40:55 -0500

    Ping pong v2
    Se usa la función ctimer en vez de clock wait, ver comentarios

--- Parcial-1/ping-pong_delay.c
+++ Parcial-1/ping-pong_delay.c
@@ -2,7 +2,8 @@
 #include "net/rime/rime.h"
 #include "random.h"
 #include "sys/node-id.h"
-#include "clock.h"
+#include "sys/ctimer.h"
+
 #include "dev/button-sensor.h"
 
 #include "dev/leds.h"
@@ -16,7 +17,22 @@ AUTOSTART_PROCESSES(&example_broadcast_process);
 
 static int message_received = 0; //Bandera, 1 cuando se recibe, 0 se inicia
 static struct broadcast_conn broadcast; //llama la función de broadcast
+static struct ctimer rebroadcast_timer; // estructura para llamar el delay
+static uint8_t pending_rebroadcast; //donde se guarda el mensaje mientras pasa el delay
+static uint8_t rebroadcast_scheduled = 0; //bandera para controlar los eventos y mensajes en cola
 /*---------------------------------------------------------------------------*/
+static void
+do_rebroadcast(void *ptr) // la función para enviar los mensajes con delay
+{
+  packetbuf_copyfrom(&pending_rebroadcast, 1); //guarda el mensaje a enviar en el buffer
+  broadcast_send(&broadcast); //inicia el broadcast
+
+  int node_direction = linkaddr_node_addr.u8[0];//Vuelve a obtener el id del nodo
+  printf("Message sent: {%d.%u}\n", node_direction, pending_rebroadcast); // Manda el nuevo mensaje con el id del nodo nuevo, es decir el que responde y con el nuevo mensaje (el contador iniciado) con delay
+
+  rebroadcast_scheduled = 0; //baja la bandera cuando se manda
+}
+
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from) // Función de callback, se define con *c la conexión, y con *from de dónde viene
 {
@@ -27,15 +43,16 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from) // Función de 
   //        from->u8[0], from->u8[1], (uint8_t)packetbuf_dataptr());
   printf("Message received: {%d.%u}\n",from->u8[0], recv_data[0]); // identifica el id del nodo y el contador de mensajes
   
-  uint8_t new_message_received = recv_data[0] + 1; //aumenta el contador del ping pong
-  clock_wait(CLOCK_SECOND / 2); 
-  packetbuf_copyfrom(&new_message_received, 1); // Reemplaza el valor del contador
-  broadcast_send(&broadcast); // Vuelve a mandar el mensaje con los contadores actualizadas
-  
-  int node_direction = linkaddr_node_addr.u8[0]; //Vuelve a obtener el id del nodo
+  uint8_t new_message_received = recv_data[0] + 1; //es la que recibe el mensaje y le aumenta uno al contador
+  pending_rebroadcast = new_message_received; // iguala el mensaje con delay con el mensaje "recibido"
 
-  printf("Message sent: {%d.%u}\n", node_direction, new_message_received); // Manda el nuevo mensaje con el id del nodo nuevo, es decir el que responde y con el nuevo mensaje (el contador iniciado)
-  
+  if(!rebroadcast_scheduled) { //revisa si hay algún mensaje en cola con delay, si no hay ! se crea uno, si hay no se crea ninguno
+    rebroadcast_scheduled = 1; //levanta la bandera
+
+    clock_time_t delay = (CLOCK_SECOND / 5) + (random_rand() % (CLOCK_SECOND / 3)); //calcula el delay, se agrega el jitter "(random_rand() % (CLOCK_SECOND / 3)" para evitar que se sobrelape la transmición
+    ctimer_set(&rebroadcast_timer, delay, do_rebroadcast, NULL); //detecta el timer, espera el delay, después del delay, vuelve a enviar el mensaje, devuelve un null al callback pq no usamos el ptr
+  }
+
   message_received = 1; //eleva la bandera, para salir del proceso
   leds_off(LEDS_GREEN); //apaga el led
 }
